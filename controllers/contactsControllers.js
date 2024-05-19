69f042206df85b34561398edac0329d740617fb8
vitalii-22 <smulka69@gmail.com>
2024-05-19 09:54:27 +0300

    .

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,8 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = (req, res) => {
+  res.send;
+};
 
 export const getOneContact = (req, res) => {};
 
